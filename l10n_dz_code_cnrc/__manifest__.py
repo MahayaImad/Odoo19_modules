b725515d0ecfb22dd7a0215a9ca19057496efac3
MahayaImad <mah_imad@yahoo.fr>
2025-11-24 00:10:48 +0100

    cnrc

---
+++ l10n_dz_code_cnrc/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Nomenclature des Activités Économiques CNRC - Algérie",
+    'description': """ Intégration des codes d'activité algériens. """,
+    'summary': """ Nomenclature des Activités Économique CNRC """,
+
+
+    'category': 'Accounting/accounting',
+    'version': '16.0.2.0',
+
+    "contributors": [
+        "1 <Chems Eddine SAHININE>",
+        "2 <Fatima MESSADI>",
+    ],
+
+    'sequence': 1,
+
+    
+    'author': 'Elosys',
+    'website': 'https://www.elosys.net',
+    'live_test_url':"https://www.elosys.net/shop/comptabilite-et-factures-algerie-2?category=6#attr=4",
+
+    "license": "LGPL-3",
+    "price": 27.50,
+    "currency": 'EUR',
+
+
+    
+    'depends': [
+        'base',
+        'l10n_dz_elosys',
+    ],
+
+    'data': [
+      
+        'data/activity_code_data.xml',
+
+    ],
+
+
+    'images': ['images/banner.gif'],
+   
+    'installable': True,
+    'auto_install': False,
+    'application':False,
+}
