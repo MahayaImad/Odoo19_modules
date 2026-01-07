0e915be6fac645bb29cec6c43d7759283d46baa4
MahayaImad <mah_imad@yahoo.fr>
2026-01-07 10:07:03 +0100

    delete some unitilisable modules

--- l10n_dz_cpss/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import template_dz
-from . import forme_juridique
-from . import res_company
-from . import res_partner
-from . import activity_code
